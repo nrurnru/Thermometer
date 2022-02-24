26d473ab5e259bc9a92bb5a9d4daf089fa5ad4ed
nrurnru <nrurnru@gmail.com>
2022-02-24 23:56:04 +0900

    Initial Commit

---
+++ Shared/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Shared
+//
+//  Created by 최광현 on 2022/02/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Shared/ThermometerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ThermometerApp.swift
+//  Shared
+//
+//  Created by 최광현 on 2022/02/24.
+//
+
+import SwiftUI
+
+@main
+struct ThermometerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
